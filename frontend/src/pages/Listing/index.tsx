05ac05cdd985e057b34ffb93472654572c5bff9a
luiz barros <luizbarro82@gmail.com>
2022-01-11 22:08:55 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,12 +1,45 @@
 import Pagination from "componentes/Pagination";
 import './styles.css'
-function Listing(){
+import MovieCard from "componentes/MovieCard";
+function Listing() {
 
-return(
+    return (
+        <>
+            <Pagination />
 
-<Pagination />
+            <div className="container">
+            <div className="row" >
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                <MovieCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3  mb-3">
+                <MovieCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3  mb-3">
+                <MovieCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3  mb-3">
+                <MovieCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3  mb-3">
+                <MovieCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3  mb-3">
+                <MovieCard />
+                </div>
+               
+            </div>
 
-)
+            </div>
+
+            
+            
+
+          
+
+        </>
+
+    )
 
 
 
