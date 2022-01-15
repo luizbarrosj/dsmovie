b3aa468b912900e73177424aea4d15278bf03754
luiz barros <luizbarro82@gmail.com>
2022-01-15 14:51:17 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -34,13 +34,16 @@ useEffect(() =>{
 
     })},[pageNumber])
 
-    
+    const handlePageChange = (newPageNumber: number)=>{
+        setPageNumber(newPageNumber)
+
+    }
     
 
     return (
         <>
        
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange} />
 
             <div className="container">
             <div className="row" >
