8ad5146182e50ca98779d46b2e5efe308ffab2db
luiz barros <luizbarro82@gmail.com>
2022-01-15 14:15:59 -0300

    useParams

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -24,7 +24,7 @@ const[page,setPage]=useState<MoviePage>({
 );
 
 useEffect(() =>{
-    axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
+    axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
     .then(response =>{
         const data = response.data as MoviePage;
         setPage(data);
