0d625b8cf4ccb383c8275f06b5f3158161197a06
luiz barros <luizbarro82@gmail.com>
2022-01-15 13:12:32 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,21 @@
 import Pagination from "componentes/Pagination";
 import './styles.css'
 import MovieCard from "componentes/MovieCard";
+import axios from "axios";
+import { BASE_URL } from "utils/request";
 function Listing() {
+
+    //Forma errada
+    axios.get(`${BASE_URL}/movies?size=12&page=1`)
+    .then(response =>{
+   console.log(response.data);
+
+    }
+
+
+
+    )
+
 
     return (
         <>
